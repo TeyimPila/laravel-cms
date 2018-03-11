bd7a8728c04d95ca82952cf49de0e91f3c55e46a
Teyim Pila <teyim.pila@gmail.com>
2018-03-11 17:51:18 +0100

    implement CRUD for permissions

--- public/js/app.js
+++ public/js/app.js
@@ -1038,6 +1038,7 @@ __webpack_require__(12);
 window.Vue = __webpack_require__(35);
 
 
+
 Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
@@ -1046,7 +1047,21 @@ var app = new Vue({
     el: '#app',
     data: {
         auto_password: true,
-        password_option: 'keep'
+        password_option: 'keep',
+        permissionType: 'basic',
+        resource: '',
+        crudSelected: ['create', 'read', 'update', 'delete']
+    },
+    methods: {
+        crudName: function crudName(item) {
+            return item.substr(0, 1).toUpperCase() + item.substr(1) + " " + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+        },
+        crudSlug: function crudSlug(item) {
+            return item.toLowerCase() + "-" + app.resource.toLowerCase();
+        },
+        crudDescription: function crudDescription(item) {
+            return "Allow a User to " + item.toUpperCase() + " a " + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+        }
     }
 });
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Buefy from 'buefy';
+
 Vue.use(Buefy);
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
@@ -11,6 +12,20 @@ var app = new Vue({
     el: '#app',
     data: {
         auto_password: true,
-        password_option: 'keep'
+        password_option: 'keep',
+        permissionType: 'basic',
+        resource: '',
+        crudSelected: ['create', 'read', 'update', 'delete']
+    },
+    methods: {
+        crudName: function (item) {
+            return item.substr(0, 1).toUpperCase() + item.substr(1) + " " + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+        },
+        crudSlug: function (item) {
+            return item.toLowerCase() + "-" + app.resource.toLowerCase();
+        },
+        crudDescription: function (item) {
+            return "Allow a User to " + item.toUpperCase() + " a " + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+        }
     }
 });
