586cb41942701ba4d371567dd92fdf4639360900
Teyim Pila <teyim.pila@gmail.com>
2018-03-11 16:46:17 +0100

    implement first version of the roles and permissions management features

--- resources/views/manage/permissions/create.blade.php
+++ resources/views/manage/permissions/create.blade.php
@@ -7,7 +7,6 @@
                 <h1 class="title">Create New Permission</h1>
             </div>
         </div>
-
         <hr class="m-t-0">
 
         <div class="columns">
@@ -17,46 +16,41 @@
 
                     <div class="block">
                         <b-radio-group v-model="permissionType">
-                            <b-radio name="permission_type" value="basic" selected>Basic Permission</b-radio>
+                            <b-radio name="permission_type" value="basic">Basic Permission</b-radio>
                             <b-radio name="permission_type" value="crud">CRUD Permission</b-radio>
                         </b-radio-group>
                     </div>
 
-
-                    <div class="field" v-if="permissionType !='basic'">
+                    <div class="field" v-if="permissionType == 'basic'">
                         <label for="display_name" class="label">Name (Display Name)</label>
                         <p class="control">
                             <input type="text" class="input" name="display_name" id="display_name">
                         </p>
                     </div>
 
-
-                    <div class="field" v-if="permissionType !='basic'">
+                    <div class="field" v-if="permissionType == 'basic'">
                         <label for="name" class="label">Slug</label>
                         <p class="control">
                             <input type="text" class="input" name="name" id="name">
                         </p>
                     </div>
 
-
-                    <div class="field" v-if="permissionType !='basic'">
+                    <div class="field" v-if="permissionType == 'basic'">
                         <label for="description" class="label">Description</label>
                         <p class="control">
-                            <input type="text" class="input" name="description" id="description"
-                                   placeholder="describe what this permission does">
+                            <input type="text" class="input" name="description" id="description" placeholder="Describe what this permission does">
                         </p>
                     </div>
 
                     <div class="field" v-if="permissionType == 'crud'">
                         <label for="resource" class="label">Resource</label>
                         <p class="control">
-                            <input type="text" class="input" name="resource" id="resource" v-model="resource"
-                                   placeholder="The name of the resource">
+                            <input type="text" class="input" name="resource" id="resource" v-model="resource" placeholder="The name of the resource">
                         </p>
                     </div>
 
                     <div class="columns" v-if="permissionType == 'crud'">
-                        <div class="columns">
+                        <div class="column is-one-quarter">
                             <b-checkbox-group v-model="crudSelected">
                                 <div class="field">
                                     <b-checkbox custom-value="create">Create</b-checkbox>
@@ -71,21 +65,18 @@
                                     <b-checkbox custom-value="delete">Delete</b-checkbox>
                                 </div>
                             </b-checkbox-group>
-                        </div>
-
+                        </div> <!-- end of .column -->
 
                         <input type="hidden" name="crud_selected" :value="crudSelected">
 
                         <div class="column">
-                            <table class="table">
+                            <table class="table" v-if="resource.length >= 3">
                                 <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Slug</th>
-                                    <th>Description</th>
-                                </tr>
+                                <th>Name</th>
+                                <th>Slug</th>
+                                <th>Description</th>
                                 </thead>
-                                <tbody v-if="resource.length >= 3">
+                                <tbody>
                                 <tr v-for="item in crudSelected">
                                     <td v-text="crudName(item)"></td>
                                     <td v-text="crudSlug(item)"></td>
@@ -95,12 +86,13 @@
                             </table>
                         </div>
                     </div>
+
                     <button class="button is-success">Create Permission</button>
                 </form>
             </div>
         </div>
-    </div>
 
+    </div> <!-- end of .flex-container -->
 @endsection
 
 @section('scripts')
@@ -110,17 +102,17 @@
             data: {
                 permissionType: 'basic',
                 resource: '',
-                crudeSelected: ['create', 'read', 'update', 'delete']
+                crudSelected: ['create', 'read', 'update', 'delete']
             },
             methods: {
-                crudName: function (item) {
-                    return item.substr(0, 1).toUpperCase() + item.substr(1) + " " + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+                crudName: function(item) {
+                    return item.substr(0,1).toUpperCase() + item.substr(1) + " " + app.resource.substr(0,1).toUpperCase() + app.resource.substr(1);
                 },
-                slugName: function (item) {
-                    return item.toLowerCase(item) + "-" + app.resource.toLowerCase();
+                crudSlug: function(item) {
+                    return item.toLowerCase() + "-" + app.resource.toLowerCase();
                 },
-                crudDescription: function () {
-                    return "Allow a user to " + item.toUpperCase() + ' a ' + app.resource.substr(0, 1).toUpperCase() + app.resource.substr(1);
+                crudDescription: function(item) {
+                    return "Allow a User to " + item.toUpperCase() + " a " + app.resource.substr(0,1).toUpperCase() + app.resource.substr(1);
                 }
             }
         });
